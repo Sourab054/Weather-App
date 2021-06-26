fc57821d5fdff3ab8c4e52b4872f0d610ae786b6
Sourab Patil <sourab054@gmail.com>
2021-06-26 18:47:17 +0530

    weather complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const apiKey = `cc5f8464753bb4f57ea0d867acfe536e`;
+const urlBase = "api.openweathermap.org/data/2.5/"; //weather?q=London&appid={API key}
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherdata, setweatherData] = useState({});
+
+  const getDate = new Date();
+  const currentDate = getDate.toDateString();
+
+  const fetchData = async (e) => {
+    try {
+      if (e.key === "Enter") {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cc5f8464753bb4f57ea0d867acfe536e&units=metric`
+        );
+        console.log(res.data);
+        setweatherData(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            onKeyPress={fetchData}
+          />
+        </div>
+        {typeof weatherdata.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weatherdata.name},{weatherdata.sys.country}
+              </div>
+              <div className="date">{currentDate}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weatherdata.main.temp)}°c</div>
+              <div className="weather"></div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
     </div>
   );
 }
